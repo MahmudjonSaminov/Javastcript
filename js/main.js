19f6100957c590e4ac06df4eac4a9fad6826431b
Mahmudjon <saminovmahmudjon@gmail.com>
2025-02-12 21:59:31 -0700

    UPDATE

--- js/main.js
+++ js/main.js
@@ -1,150 +1,150 @@
-// 1)
+1)
 
 
-// var a = 4
-// var b = 5
-// var c = 7
-// var perimetr1 = a+b+c
+var a = 4
+var b = 5
+var c = 7
+var perimetr1 = a+b+c
 
-// console.log("birinchi ucburchakning perimetri", perimetr1, "ga teng");
-
-
-
-// var a = 6
-// var b = 8
-// var c = 10
-// var perimetr2 = a+b+c
-
-// console.log("ikkinchi ucburchakning perimetri", perimetr2, "ga teng");
+console.log("birinchi ucburchakning perimetri", perimetr1, "ga teng");
 
 
 
-// var a = 3
-// var b = 4
-// var c = 5
-// var perimetr3 = a+b+c
+var a = 6
+var b = 8
+var c = 10
+var perimetr2 = a+b+c
 
-// console.log("uchinchi ucburchakning perimetri", perimetr3, "ga teng");
-// var uchta_uchburchak_yigindisi = perimetr1+perimetr2+perimetr3
-
-// console.log("3 ta uchburchakning perimetrlari  yigindisi", perimetr1+perimetr2+perimetr3, "ga teng");
+console.log("ikkinchi ucburchakning perimetri", perimetr2, "ga teng");
 
 
 
+var a = 3
+var b = 4
+var c = 5
+var perimetr3 = a+b+c
 
-// console.log("uchta uchburchakning orta arifmetigi", uchta_uchburchak_yigindisi/3, "ga teng");
+console.log("uchinchi ucburchakning perimetri", perimetr3, "ga teng");
+var uchta_uchburchak_yigindisi = perimetr1+perimetr2+perimetr3
+
+console.log("3 ta uchburchakning perimetrlari  yigindisi", perimetr1+perimetr2+perimetr3, "ga teng");
 
 
 
 
-
-
-
-// 2)
-// var a = 10 
-// console.log("torburchakning bitta tomoni", a, "ga teng" );
-// console.log("torburchakning yuzi", a*a, "ga teng" );
-
-
-
-// 3)
-// var ism = prompt("Ismingizni kiriting");
-// var yosh = +prompt("Yoshingizni kiriting");
-// console.log("sizning ismingiz", ism, "va", 2024-yosh, "chi yilda tuglgansiz" );
+console.log("uchta uchburchakning orta arifmetigi", uchta_uchburchak_yigindisi/3, "ga teng");
 
 
 
 
-// 5)
-// var son1 = +prompt("birinchi sonni kiriting" )
-// var son2 = +prompt("ikkinchi sonni kiriting" )
-// var son3 = +prompt("uchinchi sonni kiriting" )
-// var kopayt = son1*son2*son3
-// console.log("sizning sonlaringizni kopaytmasi", kopayt, "ma ga teng");
-
-// var qosh = son1+son2+son3
-// console.log("sizning sonlaringizni qoshilgan yigindisi", qosh, "ga teng");
-
-// var arifme = son1+son2+son3
-// console.log( "sizning sonlaringizni orta arifmeigi", arifme/3, "ga teng");
-
-
-// 6)
-// var a = 8 
-// console.log("bogning bitta tomoni", a, "ga teng" );
-// console.log("bogning yuzi", a*a, "ga teng" );
-// console.log("bogning perimetri", a*4, "ga teng");
-
-
-// 7)
-// var ism = prompt("Ismingizni kiriting");
-// var yosh = prompt("Yoshingizni kiriting");
-// var kasb = prompt("kasbingizni kiriting");
-// console.log("sizning ismingiz", ism, 2024-yosh, "chi yilda tuglgansiz", "va", kasb, "lik kasbida ishalysiz");
-
-
-// 8)
-// var yil = prompt("Yilingizni kiriting")
-// console.log("siz 2030-yilda " ,2030-yil,"yosh bolasiz" );
 
 
 
-// 9)
-// var son1 = +prompt("birinchi sonni kiriting" )
-// var son2 = +prompt("ikkinchi sonni kiriting" )
-// var son3 = +prompt("uchinchi sonni kiriting" )
-// var kopayt = son1*son2*son3
-// console.log("sizning sonlaringizni kopaytmasi", kopayt, "ma ga teng");
-
-// var qosh = son1+son2+son3
-// console.log("sizning sonlaringizni qoshilgan yigindisi", qosh, "ga teng");
-
-// var arifme = son1+son2+son3
-// console.log( "sizning sonlaringizni orta arifmeigi", arifme/3, "ga teng");
+2
+var a = 10 
+console.log("torburchakning bitta tomoni", a, "ga teng" );
+console.log("torburchakning yuzi", a*a, "ga teng" );
 
 
 
-// 10)
-// var son1 = +prompt("birinchi sonni kiriting faqat 0 emas" )
-// var son2 = +prompt("ikkinchi sonni kiriting faqat 0 emas" )
-// var kopayt = son1*son2
-// console.log("sizning sonlaringizni kopaytmasi", kopayt, "ma ga teng");
-
-// var qosh = son1+son2
-// console.log("sizning sonlaringizni qoshilgan yigindisi", qosh, "ga teng");
-
-// console.log("siz oylagan birinchi sonni kvadrati", son1*son1, "ga teng");
-
-// console.log("siz oylagan ikkinchi sonni kvadrati", son2*son2, "ga teng");
-
-
-// 11)
-// var a = 3
-// var b = 4
-// var c = 5
-// var perimetr1 = a+b+c
-
-// console.log("birinchi ucburchakning perimetri", perimetr1, "ga teng");
-
-// var a = 6
-// var b = 8
-// var c = 10
-// var perimetr2 = a+b+c
-
-// console.log("ikkinchi ucburchakning perimetri", perimetr2, "ga teng");
-
-// var uchta_uchburchak_yigindisi = perimetr1+perimetr2
-
-// console.log("2 ta uchburchakning perimetrlari  yigindisi", perimetr1+perimetr2, "ga teng");
-
-// console.log("2 uchburchakning orta arifmetigi", uchta_uchburchak_yigindisi/2, "ga teng");
+3
+var ism = prompt("Ismingizni kiriting");
+var yosh = +prompt("Yoshingizni kiriting");
+console.log("sizning ismingiz", ism, "va", 2024-yosh, "chi yilda tuglgansiz" );
 
 
 
 
-// 12)
-// var yosh = prompt("yoshingizni kiriting")
-// console.log("Siz 50 ga kirishingizga", 50-yosh, "yil qoldi");
+5
+var son1 = +prompt("birinchi sonni kiriting" )
+var son2 = +prompt("ikkinchi sonni kiriting" )
+var son3 = +prompt("uchinchi sonni kiriting" )
+var kopayt = son1*son2*son3
+console.log("sizning sonlaringizni kopaytmasi", kopayt, "ma ga teng");
+
+var qosh = son1+son2+son3
+console.log("sizning sonlaringizni qoshilgan yigindisi", qosh, "ga teng");
+
+var arifme = son1+son2+son3
+console.log( "sizning sonlaringizni orta arifmeigi", arifme/3, "ga teng");
+
+
+6
+var a = 8 
+console.log("bogning bitta tomoni", a, "ga teng" );
+console.log("bogning yuzi", a*a, "ga teng" );
+console.log("bogning perimetri", a*4, "ga teng");
+
+
+7
+var ism = prompt("Ismingizni kiriting");
+var yosh = prompt("Yoshingizni kiriting");
+var kasb = prompt("kasbingizni kiriting");
+console.log("sizning ismingiz", ism, 2024-yosh, "chi yilda tuglgansiz", "va", kasb, "lik kasbida ishalysiz");
+
+
+8
+var yil = prompt("Yilingizni kiriting")
+console.log("siz 2030-yilda " ,2030-yil,"yosh bolasiz" );
+
+
+
+9
+var son1 = +prompt("birinchi sonni kiriting" )
+var son2 = +prompt("ikkinchi sonni kiriting" )
+var son3 = +prompt("uchinchi sonni kiriting" )
+var kopayt = son1*son2*son3
+console.log("sizning sonlaringizni kopaytmasi", kopayt, "ma ga teng");
+
+var qosh = son1+son2+son3
+console.log("sizning sonlaringizni qoshilgan yigindisi", qosh, "ga teng");
+
+var arifme = son1+son2+son3
+console.log( "sizning sonlaringizni orta arifmeigi", arifme/3, "ga teng");
+
+
+
+10
+var son1 = +prompt("birinchi sonni kiriting faqat 0 emas" )
+var son2 = +prompt("ikkinchi sonni kiriting faqat 0 emas" )
+var kopayt = son1*son2
+console.log("sizning sonlaringizni kopaytmasi", kopayt, "ma ga teng");
+
+var qosh = son1+son2
+console.log("sizning sonlaringizni qoshilgan yigindisi", qosh, "ga teng");
+
+console.log("siz oylagan birinchi sonni kvadrati", son1*son1, "ga teng");
+
+console.log("siz oylagan ikkinchi sonni kvadrati", son2*son2, "ga teng");
+
+
+11
+var a = 3
+var b = 4
+var c = 5
+var perimetr1 = a+b+c
+
+console.log("birinchi ucburchakning perimetri", perimetr1, "ga teng");
+
+var a = 6
+var b = 8
+var c = 10
+var perimetr2 = a+b+c
+
+console.log("ikkinchi ucburchakning perimetri", perimetr2, "ga teng");
+
+var uchta_uchburchak_yigindisi = perimetr1+perimetr2
+
+console.log("2 ta uchburchakning perimetrlari  yigindisi", perimetr1+perimetr2, "ga teng");
+
+console.log("2 uchburchakning orta arifmetigi", uchta_uchburchak_yigindisi/2, "ga teng");
+
+
+
+
+12
+var yosh = prompt("yoshingizni kiriting")
+console.log("Siz 50 ga kirishingizga", 50-yosh, "yil qoldi");
 
 
 
